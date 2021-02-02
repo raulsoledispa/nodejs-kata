3ca4c345cb47b5bed65c0d786292fa8c64108fea
Raul Soledispa <soledispa.raul@gmail.com>
2021-02-02 12:15:41 -0500

    Unit test from TripService and add missing functions in 'User'

--- trip-service/app/src/User.js
+++ trip-service/app/src/User.js
@@ -2,4 +2,24 @@
 
 module.exports = class User {
 
+    constructor(){
+        this._trips = [];
+        this._friends = [];
+    }
+
+    getFriends(){
+        return this._friends;
+    }
+
+    addFriend(name){
+        this._friends.push(name);
+    }
+
+    addTrip(name){
+        this._trips = name;
+    }
+
+    getTrips(){
+        return this._trips;
+    }
 }
--- trip-service/app/test/TripServiceSpec.spec.js
+++ trip-service/app/test/TripServiceSpec.spec.js
@@ -1,11 +1,53 @@
 "use strict";
 
+jest.mock('../src/UserSession');
+jest.mock('../src/TripDAO');
 let TripService = require('../src/TripService');
-
+const UserSession = require('../src/UserSession');
+const User = require('../src/User');
+const TripDAO = require('../src/TripDAO');
+let tripService;
 describe('TripService', () => {
 
-    it('return error when user not log in', () => {
-        
+    beforeEach(() => {
+        tripService = new TripService();
+    })
+
+    it('should throw on error when user is not logged in', () => {
+        UserSession.getLoggedUser.mockReturnValue(null);
+        expect(() => {
+            tripService.getTripsByUser(null)
+        }).toThrow(/User not logged in/);
     });
+
+    
+    it('should not return any trips when users are not friends', () => {
+        UserSession.getLoggedUser.mockReturnValue('Soul');
+        let user = new User();
+        user.addFriend('Raul');
+        user.addTrip('Ecuador');
+        user.addTrip('Colombia');
+
+        tripService = new TripService();
+        const friends = tripService.getTripsByUser(user);
+        expect(friends).toHaveLength(0);
+    });
+    
+
+    
+    it('should return friend trips when users are friends', () => {
+        UserSession.getLoggedUser.mockReturnValue('Soul');
+        TripDAO.findTripsByUser.mockReturnValue(['Ecuador','Colombia']);
+        
+        let user = new User();
+        user.addFriend('Raul');
+        user.addFriend('Soul');
+        user.addTrip('Ecuador');
+        user.addTrip('Colombia');
+
+        tripService = new TripService();
+        const friends = tripService.getTripsByUser(user);
+        expect(friends).toHaveLength(2);
+    })
 
 });
