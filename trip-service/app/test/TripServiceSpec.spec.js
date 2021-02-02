df47d5c501fe9e7e68b063ac4d3893206463e9c8
Raul Soledispa <soledispa.raul@gmail.com>
2021-02-02 09:10:56 -0500

    Project restructuring and add new dependencies

---
+++ trip-service/app/test/TripServiceSpec.spec.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let TripService = require('../src/TripService');
+
+describe('TripService', () => {
+
+    it('return error when user not log in', () => {
+        
+    });
+
+});
